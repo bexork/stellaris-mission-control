883056fceee3583dca18aa6035c9a6d394b047aa
Brigham Exactly <bexact@outlook.com>
2021-07-06 16:05:28 -0700

    update imageing

--- core-image.js
+++ core-image.js
@@ -1,6 +1,6 @@
 
 import { conf } from './core-conf.js'
-import { logInfo } from './core-utils.js'
+import { Exists, logInfo } from './core-utils.js'
 import child_process from 'child_process'
 import path from 'path'
 import { logWarn, EncodeJSON } from './core-utils.js'
@@ -15,15 +15,24 @@ const imageMagicMeta = (imageMagickOutput) => {
   }
 }
 
-export const imageMagickConvert = (convertArgs) => {
+export const imageMagickConvert = (inputFile, outputFile, convertArgs) => {
   // execute the imagemagick command and throw on error
-  const cmd = `${conf.imageMagickConvert} ${convertArgs.join(' ')}`
+  if (!conf.imageMagickConvert) {
+    AbortError("Mission Abort!!! ImageMagick not configured in .mission-control.yaml. expected imageMagickConvert")
+  }
+  if (!Exists(inputFile)) {
+    AbortError("Mission Abort!!! Source file does not exist!")
+  }
+  const cmd = `${conf.imageMagickConvert} "${inputFile}" ${convertArgs.join(' ')} ${outputFile}`
   const output = child_process.execSync(cmd, {
       cwd: process.cwd(),
       stdio: [0, 1, 2]
-  });
+  })
+  return {
+    inputFile,
+    meta: imageMagickMeta(output)
+  }
 }
-
 
 export const  makePng = (inputPath, nameTag = 'starkeeper', resizePct = 200, outputRoot) => {
     const inFileType = path.extname(inputPath)
@@ -34,7 +43,6 @@ export const  makePng = (inputPath, nameTag = 'starkeeper', resizePct = 200, out
                 ? path.join(outputRoot, outFileName)
                 : path.join(path.dirname(inFileName), outFileName)
     logInfo(`makePng: ${inputPath} => ${outputFile}`)
-    const command = [inFileName, '-verbose', '-resize', `${resizePct}%`, outputFile]
-    imageMagickConvert(command)
+    imageMagickConvert(inputPath, outputFile, ['-verbose', '-resize', `${resizePct}%`])
     return outFileName
 }
